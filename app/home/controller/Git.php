2121ecd87c776a7e50322564802cebc938e4eb14
Cansnow <261001642@qq.com>
2019-07-19 15:52:23 +0800

    Update Git.php

--- app/home/controller/Git.php
+++ app/home/controller/Git.php
@@ -23,7 +23,7 @@ class Git extends Base
 		if ($signature) {
 		    $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
 		    if (strcmp($signature, $hash) == 0) {
-		        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+		        echo shell_exec("cd {$path} && git pull");
 		        exit();
 		    }
 		}
